a834a2c426fcabb6f02371c79cd9040688dfcf8f
saurav7007 <souravrajsingh7007@gmail.com>
2025-02-01 12:50:09 +0530

    Used PEP rules to make the code cleaner

--- Count-Records/Script/count_records.py
+++ Count-Records/Script/count_records.py
@@ -2,52 +2,48 @@
 
 import sys
 
+
 # Define a usage function to display help information
 def usage():
-    print("""
-          Usage: python3 count_records.py <filename>
-          
-          Description:
-          This script counts the number of records in a file. 
-          A record is identified by a header line that starts with the '>' character.
-          
-          Arguments:
-          <filename>  The name of the file to process (e.g., a FASTA file).
-          
-          Example:
-          python3 count_records.py dna.example.fasta
-    """)
+    print(
+        """
+    Usage: python3 count_records.py <filename>
+
+    Description:
+    This script counts the number of records in a file. A record is
+    identified by a header line that starts with the '>' character.
+
+    Arguments:
+    <filename>  The name of the file to process (e.g., a FASTA file).
+
+    Example:
+    python3 count_records.py dna.example.fasta
+    """
+    )
+
+
+def count_seqs(filename):
+    """
+    Count the number of sequences in a FASTA file.
+    """
+    try:
+        with open(filename, "r") as file:
+            record_count = sum(1 for line in file if line[0] == ">")
+        return record_count
+    except FileNotFoundError:
+        print(f"Error: The file {filename} does not exist.")
+        sys.exit(1)
+
 
 # Main program logic
+
+
 if len(sys.argv) < 2:
     print("Error: Missing filename argument.")
     usage()
-    sys.exit()
+    sys.exit(1)
 
-# The program need the file name as the first argument
 filename = sys.argv[1]
+records = count_seqs(filename)
 
-# Opening the file if exisit or print an error message
-try:
-    file = open(filename, 'r')
-except FileNotFoundError:
-    print("Error: The file %s does not exist." %filename)
-    sys.exit()
-
-# Create a variable to store the count of the records
-recordCount = 0
-
-# Reading each line of a file one by one
-# Autoincrement the count if the new records are found
-
-for line in file:
-    print(line)
-    if line[0] == '>':
-        recordCount += 1
-    print(recordCount)
-
-# Closing the file
-file.close()
-
-# Print the output
-print('There are %d records in the %s' % (recordCount, filename)) 
+print(f"There are {records} records in {filename}.")
